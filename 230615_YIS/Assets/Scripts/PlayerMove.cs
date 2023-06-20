771fe55118204a7bd907551ce6032d7497aa052d
YoonInS <136697953+YoonInS@users.noreply.github.com>
2023-06-20 20:58:45 +0900

    좌우 이동 코드 작성
    버그는 목요일에 수정

---
+++ 230615_YIS/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    int step = 0;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (step == 1)
+        {
+            if(this.transform.position.x >= 2f)
+            {
+
+            }
+            else
+            {
+                player.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+            }
+        }
+        if(step == 2)
+            if(player.transform.position.x <= 2f)
+        {
+            player.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+        }
+    }
+
+    public void LeftMove()
+    {
+        step = 1;
+    }
+
+    public void RightMove()
+    {
+
+
+        step = 2;
+    }
+}
+
